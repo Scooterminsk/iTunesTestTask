b22126cc6d4ca38dae7211c2a1c39b3eb754d3a2
Scooterminsk <kickscooterby@gmail.com>
2022-10-07 18:55:43 +0300

    All views on the DetailAlbumViewController were added

--- iTunes_TestTask/Controllers/DetailAlbumViewController.swift
+++ iTunes_TestTask/Controllers/DetailAlbumViewController.swift
@@ -8,10 +8,80 @@
 import UIKit
 
 class DetailAlbumViewController: UIViewController {
+    
+    private let albumLogo: UIImageView = {
+       let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let albumNameLabel: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Name album"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Name artist"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let releaseDateLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Release date"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let trackCountLabel: UILabel = {
+       let label = UILabel()
+        label.text = "10 tracks"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let collectionView: UICollectionView = {
+       let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 5
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .white
+        collectionView.bounces = false
+        collectionView.register(AlbumsTableViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private var stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViews()
+        setupDelegates()
+        setConstraints()
     }
     
+    private func setupViews() {
+        
+    }
+    
+    private func setupDelegates() {
+        
+    }
+}
+
+//MARK: - SetConstraints
+
+extension DetailAlbumViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
 }
---
+++ iTunes_TestTask/Views/Cells/SongsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SongsCollectionViewCell.swift
+//  iTunes_TestTask
+//
+//  Created by Zenya Kirilov on 7.10.22.
+//
+
+import UIKit
+
+class SongsCollectionViewCell: UICollectionViewCell {
+    
+}
