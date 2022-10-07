5d189bc5478a18b49bd32d4b637729adb67e403b
Scooterminsk <kickscooterby@gmail.com>
2022-10-07 19:37:42 +0300

    All views on the UserInfoViewController were created

--- iTunes_TestTask/Controllers/UserInfoViewController.swift
+++ iTunes_TestTask/Controllers/UserInfoViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class UserInfoViewController: UIViewController {
+    
+    private let firstNameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "First name"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let secondNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Second name"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let ageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Age"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let phoneLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Phone"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let emailLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Email"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let passwordLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Password"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
         
     }
    
 
+}
+
+//MARK: - SetConstraints
+extension UserInfoViewController {
+    
+    private func setConstraints () {
+        
+    }
 }
