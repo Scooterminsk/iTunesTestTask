3ae81c1cce7713488c81aac8799d7ce69bd9d084
Scooterminsk <kickscooterby@gmail.com>
2022-10-07 19:10:03 +0300

    SongsCollectionViewCell is setted up

--- iTunes_TestTask/Views/Cells/SongsCollectionViewCell.swift
+++ iTunes_TestTask/Views/Cells/SongsCollectionViewCell.swift
@@ -9,4 +9,32 @@ import UIKit
 
 class SongsCollectionViewCell: UICollectionViewCell {
     
+    let nameSongLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraints() {
+        
+        addSubview(nameSongLabel)
+        
+        NSLayoutConstraint.activate([
+            nameSongLabel.topAnchor.constraint(equalTo: topAnchor),
+            nameSongLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            nameSongLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            nameSongLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5)
+        ])
+    }
 }
+
