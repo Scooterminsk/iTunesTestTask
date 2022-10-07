2b55fd4979f9418af0dea3a19115cc930602b315
Scooterminsk <kickscooterby@gmail.com>
2022-10-07 12:18:11 +0300

    All views on the AlbumsViewController were created

--- iTunes_TestTask/Controllers/AlbumsViewController.swift
+++ iTunes_TestTask/Controllers/AlbumsViewController.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class AlbumsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.register(AlbumsViewController.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        setupViews()
+        setupDelegates()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+    }
+    
+    private func setupDelegates() {
+        
+    }
+    
+    private func setNavigationBar() {
+       
     }
 
+}
+
+extension AlbumsViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+        ])
+        
+    }
 }
---
+++ iTunes_TestTask/Views/Cells/AlbumsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlbumsTableViewCell.swift
+//  iTunes_TestTask
+//
+//  Created by Zenya Kirilov on 7.10.22.
+//
+
+import UIKit
+
+class AlbumsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
