d9de4ac97f2d970c1e94b2b5a80dac7a142b37c4
Scooterminsk <kickscooterby@gmail.com>
2022-10-10 21:59:01 +0300

    A regular expression for email was created

--- iTunes_TestTask/Extensions/String + Extension.swift
+++ iTunes_TestTask/Extensions/String + Extension.swift
@@ -12,10 +12,12 @@ extension String {
     
     enum ValidType {
         case name
+        case email
     }
     
     enum Regex: String {
         case name = "[a-zA-Z]{1,}"
+        case email = "[a-zA-Z0-9._]+@[a-zA-Z]+//.[a-zA-Z]{2,}"
     }
     
     func isValid(validType: ValidType) -> Bool {
@@ -25,6 +27,7 @@ extension String {
         
         switch validType {
         case .name: regex = Regex.name.rawValue
+        case.email: regex = Regex.email.rawValue
         }
         
         return NSPredicate(format: format, regex).evaluate(with: self)
